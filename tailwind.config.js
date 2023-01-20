6497a713c61718e8cc55f8ea9c6fcaab651933d5
AndargWorku <andargworku8240@gmail.com>
2023-01-20 07:46:02 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily:{
+      primary:'Saira',
+    },
+    container:{
+      padding:{
+        DeFAULT:"1.5rem",
+
+      },
+    },
+    screens:{
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1280',
+    },
+    extend:{
+    colors:{
+      primary:'#23262f',
+      accent:'#286F6C',
+      grey:{
+        DEFAULT:'#919297',
+        1:'#D9D9D9',
+        2:'#E7E9EB',
+        3: '#F5F5F5',
+      
+      },
+      white:'#fff',
+      pink:'pink',
+    },
+    backgroundImage:{
+      hero:`url("/src/assets/img/hero.png")`,
+      newsletter:`url(/src/assets/img/newsletter.png)`,
+    },
+    dropShadow:{
+      primary:'0px 4px 10px rgba(15,27,51,0.05)',
+    },
+  },
+},
+  
+    plugins: [],
+  };
+  
+  
+//     extend: {},
+//   },
+  //  plugins: [],
+//  }
